c7a3d1b1dee7629cfcc3cd50f9f965435930eb95
gondar-software <monate615@gmail.com>
2025-07-11 08:37:22 +0000

    update initial frontend config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,37 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
+    react()
   ],
   resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
-    },
+    alias: [
+      { find: '@', replacement: '/src' },
+    ]
   },
-  root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: "./dist",
     emptyOutDir: true,
+    chunkSizeWarningLimit: 5120,
   },
   server: {
     fs: {
       strict: true,
       deny: ["**/.*"],
     },
+    allowedHosts: [
+      "roman-terpeliuk.portfolio-app.online",
+      "www.roman-terpeliuk.portfolio-app.online"
+    ]
   },
 });
